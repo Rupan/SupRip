4600ac32a9fe5619db229aa002661c1913e9ff6a
Michael Mohr <akihana@gmail.com>
2016-11-24 02:12:31 -0800

    Misc fixes

--- SupRip/Properties/AssemblyInfo.cs
+++ SupRip/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SupRip")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OCR application for HD-DVD and Bluray subtitles")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SupRip")]
